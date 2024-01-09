d8fe19483bfa85cab8781727f7e32e52a8920d38
ibrahimkdu <mohammad.ibrahim@kickdrumtech.com>
2024-01-10 01:01:46 +0530

    bug from qn1 removed diving bt zero

--- backend/homework/2/Qn1/src/main/java/StudentUtil.java
+++ backend/homework/2/Qn1/src/main/java/StudentUtil.java
@@ -18,6 +18,7 @@ public class StudentUtil {
                 }
                 c++;
             }
+            if(c!=0)
             sum =sum/c; // Calculate average GPA
             ans[i] =sum;
         }
